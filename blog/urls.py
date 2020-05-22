313041de836ebf2d5b3ea75137ff1b81d3306329
mattfreire <freire1996@gmail.com>
2020-05-22 14:36:16 +0200

    Static files

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -9,3 +11,7 @@ urlpatterns = [
     path('api/', include('posts.urls', namespace='posts'))
     # path('accounts/', include('allauth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
