96cd4e6d727446fd65cceb681009d578a9b84bbe
mattfreire <freire1996@gmail.com>
2020-05-22 12:07:27 +0200

    Project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Markdown Blog Project
+
+This repository holds the code for the React Markdown Blog Project on [JustDjango](https://learn.justdjango.com)
+
+This repo only contains the code for the backend, which is built with Django and the Django Rest Framework.
---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    # path('accounts/', include('allauth.urls')),
+]
